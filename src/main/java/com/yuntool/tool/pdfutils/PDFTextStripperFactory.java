c7c666d219386a9d7f3518b8ea52d95312fa8c9e
YunKkkk <dyh741657141@gmail.com>
2025-04-30 09:22:32 +0800

    refactor: PDF analyze

--- src/main/java/com/yuntool/tool/pdfutils/PDFTextStripperFactory.java
+++ src/main/java/com/yuntool/tool/pdfutils/PDFTextStripperFactory.java
@@ -4,19 +4,13 @@ import com.yuntool.tool.pdfutils.pdfparsers.*;
 
 public class PDFTextStripperFactory {
     public static MyPDFTextStripper getStripper(String type) throws Exception {
-        switch (type) {
-            case "Paragraphs":
-                return new PDFTextStripperOfParagraphs();
-            case "BigTitle":
-                return new PDFTextStripperOfBigTitle();
-            case "Zyqkzb":
-                return new PDFTextStripperOfZyqkzb();
-            case "Gbyg":
-                return new PDFTextStripperOfGbyg();
-            case "Hngzdt":
-                return new PDFTextStripperOfHngzdt();
-            default:
-                throw new IllegalArgumentException("Unknown type");
-        }
+        return switch (type) {
+            case "Paragraphs" -> new PDFTextStripperOfParagraphs();
+            case "BigTitle" -> new PDFTextStripperOfBigTitle();
+            case "Zyqkzb" -> new PDFTextStripperOfZyqkzb();
+            case "Gbyg" -> new PDFTextStripperOfGbyg();
+            case "Hngzdt" -> new PDFTextStripperOfHngzdt();
+            default -> throw new IllegalArgumentException("Unknown type");
+        };
     }
 }
